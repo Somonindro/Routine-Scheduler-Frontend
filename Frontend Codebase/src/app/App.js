bc88bc328d6da25f454098cae62d47b40d4c21d4
Somonindro <somoroy1290@gmail.com>
2024-01-29 23:44:20 +0600

    Initializing project using Create React App

---
+++ Frontend Codebase/src/app/App.js
@@ -0,0 +1,94 @@
+import axios from "axios";
+import { useEffect, useState, createContext } from "react";
+import { withRouter } from "react-router-dom";
+import "./App.scss";
+import AppRoutes from "./AppRoutes";
+import Navbar from "./shared/Navbar";
+import Sidebar from "./shared/Sidebar";
+import SettingsPanel from "./shared/SettingsPanel";
+import Footer from "./shared/Footer";
+import "./App.css";
+import { useLocation } from "react-router-dom/cjs/react-router-dom.min";
+import { FORBIDDEN, UNAUTHORIZED } from "./api";
+import { Toaster, toast } from "react-hot-toast";
+
+export const UserContext = createContext({user: undefined, setUser: u => {}});
+export const UserProvider = UserContext.Provider;
+
+function App(props) {
+  const location = useLocation();
+  const [isFullPageLayout, setIsFullPageLayout] = useState(false);
+
+  const [user, setUser] = useState({loggedIn: Boolean(localStorage.getItem("token"))});
+
+  console.log("user: ", user);
+
+  axios.interceptors.response.use(
+    (response) => response,
+    (error) => {
+      console.log(error);
+      const status = error.response.status;
+
+      if (status === UNAUTHORIZED || status === FORBIDDEN) {
+        localStorage.removeItem("token");
+        setUser({loggedIn: false});
+        return Promise.reject(error);
+      } else {
+        const message = error.response.message || "Something went wrong...";
+        toast.error(message);
+        return Promise.reject(error);
+      }
+    }
+  );
+
+  useEffect(() => {
+    onRouteChanged(location);
+  }, [location]);
+
+  function onRouteChanged(location) {
+    window.scrollTo(0, 0);
+    const fullPageLayoutRoutes = [
+      "/auth/login",
+      "/auth/forgot-password",
+      "/form/"
+    ];
+    const isFullPageLayout = fullPageLayoutRoutes.some(l => location.pathname.startsWith(l))
+    console.log("isFullPageLayout: ", isFullPageLayout, location.pathname);
+    setIsFullPageLayout(isFullPageLayout);
+
+    if (isFullPageLayout) {
+      document
+        .querySelector(".page-body-wrapper")
+        .classList.add("full-page-wrapper");
+    } else {
+      document
+        .querySelector(".page-body-wrapper")
+        .classList.remove("full-page-wrapper");
+    }
+  }
+
+  let navbarComponent = !isFullPageLayout ? <Navbar /> : "";
+  let sidebarComponent = !isFullPageLayout ? <Sidebar /> : "";
+  let SettingsPanelComponent = !isFullPageLayout ? <SettingsPanel /> : "";
+  let footerComponent = !isFullPageLayout ? <Footer /> : "";
+
+  return (
+    <UserProvider value={{ user: user, setUser }}>
+      <div className="container-scroller">
+        {navbarComponent}
+        <div className={"container-fluid page-body-wrapper"}>
+          {sidebarComponent}
+          <div className="main-panel">
+            <div className="content-wrapper">
+              <AppRoutes />
+              {SettingsPanelComponent}
+            </div>
+            {footerComponent}
+          </div>
+        </div>
+      </div>
+    </UserProvider>
+  );
+}
+
+export default withRouter(App);
---
+++ Frontend Codebase/src/app/AppRoutes.js
@@ -0,0 +1,72 @@
+import React, { Component, Suspense, lazy } from "react";
+import { Switch, Route, Redirect } from "react-router-dom";
+
+import Spinner from "../app/shared/Spinner";
+import { useContext } from "react";
+import { UserContext } from "./App";
+
+const Dashboard = lazy(() => import("./dashboard/Dashboard"));
+const Teachers = lazy(() => import("./database/Teachers"));
+const Sections = lazy(() => import("./database/Sections"));
+const Rooms = lazy(() => import("./database/Rooms"));
+const Courses = lazy(() => import("./database/Courses"));
+
+const TheoryPreference = lazy(() => import("./theory-pref/TheoryPreference"));
+const TheorySelect = lazy(() => import("./forms/TheorySelect"));
+const SessionalSelect = lazy(() => import("./forms/SessionalSelect"));
+const TheoryScheduleForm = lazy(() => import("./forms/TheorySchedule"));
+
+const TheorySchedule = lazy(() => import("./theory-schedule/AskForSchedule"));
+const FixedSchedule = lazy(() => import("./theory-schedule/FixedSchedule"));
+
+const LabRoomAssign = lazy(() => import("./lab-room-assign/LabRoomAssign"));
+const SessionalPreference = lazy(() => import("./sessional-pref/SessionalPreference"));
+const SessionalSchedule = lazy(() => import("./sessional-schedule/SessionalSchedule"));
+
+const Error404 = lazy(() => import("./error-pages/Error404"));
+const Error500 = lazy(() => import("./error-pages/Error500"));
+
+const Login = lazy(() => import("./user-pages/Login"));
+const ForgetPassword = lazy(() => import("./user-pages/ForgetPassword"));
+
+const BlankPage = lazy(() => import("./general-pages/BlankPage"));
+
+const pdfPage = lazy(() => import("./pdf/ShowPdf"));
+
+
+export default function AppRoutes() {
+  const { user } = useContext(UserContext);
+
+  return (
+    <Suspense fallback={<Spinner />}>
+      <Switch>
+        <Route exact path="/form/theory-pref/:id" component={TheorySelect} />
+        <Route exact path="/form/sessional-pref/:id" component={SessionalSelect} />
+        <Route exact path="/form/theory-sched/:id" component={TheoryScheduleForm} />
+        {user.loggedIn ? (
+          <Switch>
+            <Route exact path="/dashboard" component={Dashboard} />
+            <Route path="/database/teachers" component={Teachers} />
+            <Route path="/database/sections" component={Sections} />
+            <Route path="/database/rooms" component={Rooms} />
+            <Route path="/database/courses" component={Courses} />
+            <Route path="/theory-assign" component={TheoryPreference} />
+            <Route path="/theory-schedule/ask" component={TheorySchedule} />
+            <Route path="/theory-schedule/fixed" component={FixedSchedule} />
+            <Route path="/room-assign" component={ LabRoomAssign } />
+            <Route path="/lab-assign" component={ SessionalPreference } />
+            <Route path="/lab-schedule" component={ SessionalSchedule } />
+            <Route path="/pdf" component={pdfPage} />
+            <Redirect to="/dashboard" />
+          </Switch>
+        ) : (
+          <Switch>
+            <Route path="/auth/login" component={Login} />
+            <Route path="/auth/forgot-password" component={ForgetPassword} />
+            <Redirect to="/auth/login" />
+          </Switch>
+        )}
+      </Switch>
+    </Suspense>
+  );
+}
